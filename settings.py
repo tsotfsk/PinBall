893ffca60e00aa3b7995b2566b3796859a4094f6
tsotfsk <1337990880@qq.com>
2019-05-16 09:21:24 +0800

    配置文件

---
+++ settings.py
@@ -0,0 +1,45 @@
+# 颜色
+BLACK = (0,     0,      0)
+WHITE = (255,   255,    255)
+GREEN = (102,   255,    204)
+RED =   (255,   80,     80)
+BLUE =  (0,     153,    255)
+GREEN = (0,     255,    0)
+PINK =  (255,   0,      255)
+
+# 屏幕尺寸
+SCREENWIDTH = 500
+SCREENHEIGHT = 600
+
+# 挡板尺寸
+BAFFLEWIDTH = 150
+BAFFLEHEIGHT = 30
+
+# 挡板速度
+BAFFLESPEED = 500
+
+# 球的大小
+BALLSIZE = 30
+
+# 游戏最大FPS
+FPS = 60
+
+# 球的样式列表
+BALLFORMLIST = [
+    './image/ball/pokeball.png',
+    './image/ball/basketball_1.png',
+    './image/ball/basketball_2.png',
+    './image/ball/basketball_3.png',
+    './image/ball/basketball_4.png',
+    './image/ball/basketball_5.png',
+    './image/ball/basketball_5.png',
+    './image/ball/basketball_6.png',
+    './image/ball/basketball_7.png',
+    './image/ball/basketball_8.png',
+    './image/ball/basketball_9.png',
+    './image/ball/basketball_10.png',
+    './image/ball/basketball_11.png',
+    './image/ball/dragonball.png',
+    './image/ball/captainball.png'
+]
+
